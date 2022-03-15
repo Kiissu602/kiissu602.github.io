aecf1d0c13440fdcd6bef2df01b2b44d9d8be2bf
Phumin <poolmean602@gmail.com>
2022-03-15 23:47:25 +0700

    add picture and name

---
+++ src/components/Computer.vue
@@ -0,0 +1,23 @@
+<script>
+import Image from "./computer/image.vue";
+import Name from "./computer/name.vue";
+export default {
+  components: { Image, Name },
+};
+</script>
+
+<template>
+  <div class="container">
+    <div class="contact"><Name /><Image /></div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  height: 1080px;
+}
+.contact {
+  display: flex;
+  justify-content: space-around;
+}
+</style>
---
+++ src/components/computer/image.vue
@@ -0,0 +1,12 @@
+<template>
+  <img src="../../assets/profile_pic.jpg" alt="" class="profile" />
+</template>
+
+<style scoped>
+.profile {
+  width: 240px;
+  height: 240px;
+  border-radius: 100%;
+  border: 4px whitesmoke solid;
+}
+</style>
