fb44575f7170cba0e8e5e4a5e49d38678a771005
Phumin <poolmean602@gmail.com>
2022-03-18 23:19:46 +0700

    change cover background color

--- src/components/Desktop.vue
+++ src/components/Desktop.vue
@@ -1,15 +1,19 @@
 <script>
 import Details from "./desktop/details.vue";
 import Ico from "./desktop/ico.vue";
+import LeftSide from "./desktop/left-side.vue";
 export default {
-  components: { Details, Ico },
+  components: { Details, Ico, LeftSide },
 };
 </script>
 
 <template>
   <div class="card">
     <div class="card-title"><Details /><Ico /></div>
-    <div class="card-text">Education</div>
+    <div class="card-body">
+      <LeftSide />
+      <hr />
+    </div>
   </div>
 </template>
 
@@ -23,11 +27,33 @@ export default {
   padding: 2rem;
 }
 .card-title {
-  background-color: #283848;
+  background: #180350;
+  background: radial-gradient(
+    circle at 30% 107%,
+    #040015 0%,
+    #09012f 5%,
+    #180350 45%,
+    #650045 60%,
+    #cd006b 90%
+  );
+
   margin: -2rem;
   padding: 2rem;
   display: flex;
   border-radius: 5px 5px 0px 0px;
   justify-content: space-around;
+}
+
+.card-body {
+  margin-top: 2rem;
+  padding: 2rem 0rem 0rem 0rem;
+  display: flex;
+  justify-content: space-around;
+  height: 75%;
+}
+
+hr {
+  border-style: inset;
+  border-width: 1px;
 }
 </style>
